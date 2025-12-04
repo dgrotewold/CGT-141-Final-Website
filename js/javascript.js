8e0b5c6311c43503013d89ae2ab13d5598384512
RobertoRavioli <dgrotewo@purdue.edu>
2025-12-03 21:02:18 -0500

    rename pages (pain) and jank ass javascript search bar functionality

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+function searchFunction() {
+    var form = document.getElementById("searchForm")
+    var text = form.value.toLowerCase();
+    if (text == "spain") {
+        console.log(text);
+    } else if (text == "dubai") {
+        console.log(text);
+    } else if (text == "canada") {
+        console.log(text);
+    } else if (text == "france") {
+        console.log(text);
+    } else if (text == "hong kong") {
+        console.log(text);
+    } else if (text == "chgongqing") {
+        console.log(text);
+    }
+}
