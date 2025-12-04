ffa4977575c93816e6f4099ecf6ce57c2867ec67
RobertoRavioli <dgrotewo@purdue.edu>
2025-12-03 21:25:48 -0500

    search bar functionality

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,22 @@
 function searchFunction() {
     var form = document.getElementById("searchForm")
     var text = form.value.toLowerCase();
+    // remove any characters but letters
+    text = text.replace(/[^A-Za-z]+/g, '');
+    console.log(text);
     if (text == "spain") {
-        console.log(text);
+        location.href = "https://www.wanderlustblog.page/pages/spain.html"
     } else if (text == "dubai") {
-        console.log(text);
+        location.href = "https://www.wanderlustblog.page/pages/dubai.html"
     } else if (text == "canada") {
-        console.log(text);
+        location.href = "https://www.wanderlustblog.page/pages/canada.html"
     } else if (text == "france") {
-        console.log(text);
-    } else if (text == "hong kong") {
-        console.log(text);
-    } else if (text == "chgongqing") {
-        console.log(text);
+        location.href = "https://www.wanderlustblog.page/pages/france.html"
+    } else if (text == "hong kong" || text == "hongkong") {
+        location.href = "https://www.wanderlustblog.page/pages/hongkong.html"
+    } else if (text == "chongqing") {
+        location.href = "https://www.wanderlustblog.page/pages/chongqing.html"
+    } else if (text == "gaster") {
+        location.href = "https://www.webpagetest.org/blank.html"
     }
 }
